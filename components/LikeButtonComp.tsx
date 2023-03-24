5168854316837817a3a5aa4671f27aa0e8f718b2
THasegawa0216 <121989273+THasegawa0216@users.noreply.github.com>
2023-03-24 13:51:45 +0900

    2 files modified

--- components/LikeButtonComp.tsx
+++ components/LikeButtonComp.tsx
@@ -10,9 +10,18 @@ export default function LikeButtonComp() {
 }
 
 function LikeButtonFunc() {
+  /* React のイベントは小文字でなく、キャメルケース(onClick)で記述する
+    例）
+    onClick：クリックされた時、button タグなど
+    onSubmit：送信された時、form タグなど
+    onChange：入力や削除が行われた時、input タグなど
+    onMouseOver：マウスカーソルが上に置かれた時
+    onMouseOut：マウスカーソルが外れた時
+  */
   const onClickCountUp = () => {
     setCount(count + 1);
   };
+
   const [count, setCount] = React.useState(0);
 
   return (
